1b0e9a454e91b317ac9ca39c5f1c94e102e11f37
chizzyboss <136846320+chizzyboss@users.noreply.github.com>
2024-12-28 01:13:07 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-
-
+app.use(helmet());
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+}); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('server is running on port $ {PORT}');
+});
 
 
 
